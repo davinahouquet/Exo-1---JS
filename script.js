f13c7871c25153ad4ae7b766010f509f60d3b949
davinahouquet <davina.houquet@hotmail.com>
2023-04-13 16:23:43 +0200

    addEventListener

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//-------------------------------------------------------
 const box = document.createElement("div")
 box.classList.add("box")
 
@@ -6,11 +5,19 @@ const square = document.querySelector("#square")
 square.appendChild(box)
 box.innerText = "Click Me!"
 
+//--------------------------------------------------------------------------------
+var bgColor = window.getComputedStyle(square).getPropertyValue("background-color")
+var color = window.getComputedStyle(square).getPropertyValue("color")
+var height= window.getComputedStyle(square).getPropertyValue("height")
+var width = window.getComputedStyle(square).getPropertyValue("width")
+var display = window.getComputedStyle(square).getPropertyValue("display")
+var display1 = window.getComputedStyle(square).getPropertyValue("font-size")
+
 
 function showReaction(type, clickedSquare){
     clickedSquare.classList.add(type)
     }
 
 square.addEventListener("click", function(){
-    alert("Clicked!")
+    alert("Classe : Square \n - Background color : " + bgColor + "\n  - Color : " + color + "\n - Height :" + height + "\n - Width : " +width+ "\n - Display : " +display+ "\n - Font-size : "+display1+" ")
 })
