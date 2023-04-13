29cebba9d8df63a76797b48fc983d13d19231438
davinahouquet <davina.houquet@hotmail.com>
2023-04-13 16:04:09 +0200

    Ajout CSS + div clickable

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.getElementById("square").style.backgroundColor = black;
-document.getElementById("square").style.width = "200px";
-document.getElementById("square").style.height = "200px";
+//-------------------------------------------------------
+const box = document.createElement("div")
+box.classList.add("box")
+
+const square = document.querySelector("#square")
+square.appendChild(box)
+box.innerText = "Click Me!"
+
+
+function showReaction(type, clickedSquare){
+    clickedSquare.classList.add(type)
+    }
+
+square.addEventListener("click", function(){
+    alert("Clicked!")
+})
